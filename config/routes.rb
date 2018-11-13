fe73566fd86a2626a0f4aafaf5e8d3f61af2e4d8
adwaith <adwaith@qburst.com>
2018-11-13 19:15:12 +0530

    styling in progress

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
 
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
    
-  root to: redirect('/login')
+  root to: redirect('/register/cmon_let_me_in')
 end
