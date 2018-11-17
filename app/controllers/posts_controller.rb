7192ba3213aa20e2aa9eaa8e251c231e04d90fb6
adwaith <adwaith@qburst.com>
2018-11-17 15:32:50 +0530

    posts partial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,11 @@ class PostsController < ApplicationController
     def create
 
     end
+
+    def save_article
+      p 'helloooo'
+      p params
+      render "<h1>save article</h1>"
+    end
+
 end
