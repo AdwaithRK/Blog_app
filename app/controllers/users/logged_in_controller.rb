db2b8de9dc05c687dfcc0d639e20782ff2971745
adwaith <adwaith@qburst.com>
2018-11-21 18:14:45 +0530

    feed completed

--- app/controllers/users/logged_in_controller.rb
+++ app/controllers/users/logged_in_controller.rb
@@ -1,5 +1,13 @@
 class Users::LoggedInController < ApplicationController
     def index
-      @name=current_user.name
+      @name = current_user.name
+      @feedtype = params[:feed]
+      p params[:feed]
+      if(@feedtype == 'public')
+        @posts = Post.where(privacy: "Publicly", status: "Publish").includes(:user);
+      else 
+        @posts = current_user.posts
+      end
+
     end
 end
