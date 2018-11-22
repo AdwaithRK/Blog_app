40aea0ef4525b5883dc14996177c666cb8a69bdd
adwaith <adwaith@qburst.com>
2018-11-22 11:19:31 +0530

    comment model created

---
+++ app/models/comment.rb
@@ -0,0 +1,7 @@
+class Comment < ApplicationRecord
+    belongs_to :post
+    belongs_to :user
+    validates :content, presence: true
+    has_many :children, class_name: "Comment", foreign_key: "parent_id"
+    belongs_to :parent, class_name: "Comment", optional: true
+end
